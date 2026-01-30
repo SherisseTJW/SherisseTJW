d28f9d681270aee36fbc092a4160827bbfa8b1a1
SherisseTJW <tanjingwensherisse@gmail.com>
2026-01-30 20:27:06 +0800

    feat: Basic structure (pdf not working)

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PDFViewer from 'pdf-viewer-reactjs';
+
+const Resources = () => {
+    return (
+        <embed src="/path/to/your/pdf" width="100%" height="600px" />
+    )
+}
+
+export default Resources
